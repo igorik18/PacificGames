973ef051256eece9559fbb58f96f7b34710435ea
Игорь Завалов <izavalov@mail.ru>
2021-12-14 13:50:52 +0300

    zava puzzle
    puzzle game with numbers

--- level.py
+++ level.py
@@ -6,6 +6,7 @@
 from tkinter import *
 from PIL import Image, ImageTk
 import re
+
 __author__ = "Hailey Johnson, Krikor Herlopian, Charitha Sree Jayaramireddy , Syrina Haldiman and Tatiyana Bramwell"
 __copyright__ = "Copyright 2021, University of New Haven Final Project"
 
@@ -18,45 +19,45 @@ __copyright__ = "Copyright 2021, University of New Haven Final Project"
 	Level 5 means 10*10
 """
 
+
 def center_window(width=250, height=100):
-	"""
+    """
 		center screen on window
 	"""
-		
-	# get screen width and height
-	screen_width = master.winfo_screenwidth()
-	screen_height = master.winfo_screenheight()	
-	# calculate position x and y coordinates
-	x = (screen_width/2) - (width/2)
-	y = (screen_height/2) - (height/2)
-	master.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
+    # get screen width and height
+    screen_width = master.winfo_screenwidth()
+    screen_height = master.winfo_screenheight()
+    # calculate position x and y coordinates
+    x = (screen_width / 2) - (width / 2)
+    y = (screen_height / 2) - (height / 2)
+    master.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
 
 master = Tk()
 master.title("Set - Level")
-center_window(407,85)
+center_window(407, 85)
 
 
 def onClick(i):
-    #update lev.txt file with new level number.
+    # update lev.txt file with new level number.
     with open("lev.txt", "w") as f:
         f.write(str(i))
-    print(str(i))#this returns value to caller
+    print(str(i))  # this returns value to caller
     master.destroy()
     return
 
 
-
 u = 10
-level = [1,2,3,4,5]
+level = [1, 2, 3, 4, 5]
 l = 0
 
-for i in range(1,6):
-    level_button = Button(master,compound = "center",text = level[l], width = 3, height = 1,font=('arial 15 bold'),highlightbackground = "light blue",bg = "light blue", fg = 'midnight blue', command=lambda i=i: onClick(i))
-    level_button.place(x = u, y = 10)
+for i in range(1, 6):
+    level_button = Button(master, compound="center", text=level[l], width=3, height=1, font=('arial 15 bold'),
+                          highlightbackground="light blue", bg="light blue", fg='midnight blue',
+                          command=lambda i=i: onClick(i))
+    level_button.place(x=u, y=10)
     l = l + 1
     u = u + 90
-
 
 mainloop()
